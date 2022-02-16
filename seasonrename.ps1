6d9a4aa6ff1370703c8988995eddd70ca7f82c5d
Greg Pendleton <gpendleton@gmail.com>
2022-02-16 14:46:24 -0600

    Delete seasonrename.ps1

--- seasonrename.ps1
+++
@@ -1,9 +0,0 @@
-$files = get-childitem 'C:\video\once upon a time\season 01'
-foreach ($file in $files){
-    $fileNameMeat = ($file.name).Split(' - ')[-1]
-    if ($fileNameMeat.startswith('S02')){
-        $oldName = $fileNameMeat
-        $newName = $oldName.Replace('02','01')
-        Rename-Item -Path $($file.FullName) -NewName "Once Upon a Time - $newName"
-    }
-}
