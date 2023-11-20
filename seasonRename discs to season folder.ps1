3694f7cf1be259e28c0475d13be7ecd3ef6d3ee1
Greg Pendleton <gpendleton@gmail.com>
2023-11-20 11:39:11 -0600

    adds change file function

--- seasonRename discs to season folder.ps1
+++ seasonRename discs to season folder.ps1
@@ -1,7 +1,7 @@
 
 # get the main video storage path and if nothing is entered assume c:\video
 $basePath = Read-Host -Prompt 'video folder (press enter for [c:\video])'
-if ([string]::isNullOrWhiteSpace($basePath)){
+if ([string]::isNullOrWhiteSpace($basePath)) {
     $basePath = "c:\video"
 }
 # get the show name
@@ -14,19 +14,19 @@ $seasonNumber = "{0:d2}" -f $seasonNumber
 $seasonPath = "$basePath\$showName\season $seasonNumber\"
 # setup the log file
 $logFile = "${basepath}\logs\$(get-date -Format "yyyy-MM-dd_HH.mm")_seasonmove.log"
-if (!(Test-Path $logFile)){
-    if (!(Test-Path "${basepath}\logs\")){
+if (!(Test-Path $logFile)) {
+    if (!(Test-Path "${basepath}\logs\")) {
         New-Item -ItemType Directory -Path "${basepath}\logs"
     }
     new-item -ItemType File -Path $logFile
 }
 
 # create the season folder if it doesn't exist, with approval
-if (!(test-path $seasonPath)){
+if (!(test-path $seasonPath)) {
     $folderConfirmation = read-host -Prompt "($seasonPath) does not exist. Create it? (y/n)"
-    if ($folderConfirmation -eq 'y'){
+    if ($folderConfirmation -eq 'y') {
         new-item -ItemType directory -path $seasonPath #-WhatIf
-        "Directory $seasonPath does not exist and was created`n"| Out-File -FilePath logFile -Append
+        "Directory $seasonPath does not exist and was created`n" | Out-File -FilePath logFile -Append
     }
     elseif ($folderConfirmation -ne 'y') {
         exit
@@ -35,7 +35,7 @@ if (!(test-path $seasonPath)){
 
 #setup initial defaults
 $episodeNumber = 01 # starting episode number
-$discNumber = 1
+$discNumber = 01
 $directories = Get-ChildItem $seasonPath -Directory
 
 # iterate through the disc folders
@@ -45,20 +45,20 @@ foreach ($folder in $directories) {
         $files = get-childitem $folder -Recurse -file
 
         # iterate through files
-        foreach ($file in $files){
+        foreach ($file in $files) {
 
             # verify it has the right extensions
-            if (((get-item $($file.FullName)).Extension) -eq '.mkv'){
+            if (((get-item $($file.FullName)).Extension) -eq '.mkv') {
                 
                 # rename and move stuff
                 $episodeNumber = "{0:d2}" -f $episodeNumber
                 $sequence = "$showName - S$($seasonNumber)E$episodeNumber"
-                #copy-Item -Path $($file.FullName) -destination "$seasonPath\$sequence.mkv" #-WhatIf
-                move-Item -Path $($file.FullName) -destination "$seasonPath\$sequence.mkv" #-WhatIf
+                copy-Item -Path $($file.FullName) -destination "$seasonPath\$sequence.mkv" #-WhatIf
+                #move-Item -Path $($file.FullName) -destination "$seasonPath\$sequence.mkv" #-WhatIf
                 $episodeNumber = [int]$episodeNumber
                 $episodeNumber ++
-                # "File $($file.FullName) renamed and coppied to $($seasonPath)$($sequence).mkv" | Out-File -FilePath $logFile -Append
-                "File $($file.FullName) renamed and moveded to $($seasonPath)$($sequence).mkv" | Out-File -FilePath $logFile -Append
+                "File $($file.FullName) renamed and coppied to $($seasonPath)$($sequence).mkv" | Out-File -FilePath $logFile -Append
+                #"File $($file.FullName) renamed and moveded to $($seasonPath)$($sequence).mkv" | Out-File -FilePath $logFile -Append
         
             }
         
@@ -71,6 +71,26 @@ foreach ($folder in $directories) {
 
 
 
-
+function processFile {
+    param (
+        [string]$file,
+        [string]$showPath,
+        [number]$seasonNumber,
+        [string]$logFile,
+        [bool]$shouldMoveFile,
+        [bool]$isTest
+    )
+    ($moveFile) ? (& { $command = 'Move-Item'; $resultText = 'moved' }) :
+     (& { $command = 'copy-Item'; $resultText = 'coppied' })
+    ($isTest) ? ($testCommand = "-whatIf") : ($testCommand = "")
+    
+    # rename and move stuff
+    $episodeNumber = "{0:d2}" -f $episodeNumber
+    $sequence = "$showName - S$($seasonNumber)E$episodeNumber"
+    &"$($command) -Path $($file.FullName) -destination $($seasonPath)\$($sequence.mkv) $($testCommand)"
+    $episodeNumber = [int]$episodeNumber
+    $episodeNumber ++
+    "File $($file.FullName) renamed and $($resultText) to $($seasonPath)$($sequence).mkv" | Out-File -FilePath $logFile -Append
+}
 
 
